891d15b1483f471a66dc6d865f9e755f51a38388
apurv2603 <92771728+apurv2603@users.noreply.github.com>
2026-01-12 08:41:44 +0400

    test change

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ for (const folder of commandFolders) {
     }
   }
 }
-
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
   const command = interaction.client.commands.get(interaction.commandName);
